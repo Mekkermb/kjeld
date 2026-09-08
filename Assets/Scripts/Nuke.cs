3b4900ede77de35bbfe898b5aa3adfa1fc782b8b
Rasmus Jensen <github@rasmus.zip>
2026-09-08 10:28:58 +0200

    add some headers ?

--- Assets/Scripts/Nuke.cs
+++ Assets/Scripts/Nuke.cs
@@ -5,12 +5,15 @@ public class Nuke : MonoBehaviour
 {
     public InputActionReference InputAction;
 
+    [Header("Scale")]
     public float OutwardSpeed = 2.5f;
     public float StopAt = 10.0f;
 
+    [Header("Nuke Cooldown")]
     public float NukeCooldown = 30.0f;
     public float NukeCooldownRemaining = 0.0f;
 
+    [Header("State")]
     [SerializeField] private Vector2 Source;
     [SerializeField] private float Progress = 0f;
     private GameObject[] fires;
