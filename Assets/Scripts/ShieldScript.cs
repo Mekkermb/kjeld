15b2c1ed0d461bc74caf9cd2938c80734d2cccb4
Rasmus Jensen <github@rasmus.zip>
2026-09-08 08:34:27 +0200

    fire collision

---
+++ Assets/Scripts/ShieldScript.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+    GameObject Manager;
+    ObjectPooler objectPooler;
+    void Start()
+    {
+        Manager = GameObject.FindGameObjectWithTag("GameController");
+        ObjectPooler objectPooler = Manager.GetComponent<ObjectPooler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Fire"))
+        {
+            objectPooler.DestroyObject(collision.gameObject);
+
+            // fire score increment here
+        }
+    }
+}
