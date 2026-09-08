c81294b605146c2bcd299a5a6f3fa6f68f35d89c
DerpGuts <347168@via.dk>
2026-09-08 15:19:39 +0200

    Add MainMenu UI and StartGame handler
    Add runtime UI for the main menu: Canvas, EventSystem, a StartGame Button (with TMP label) and associated CanvasRenderer/RectTransforms in MainMenu.unity. Wire the button's OnClick to MainMenu.StartGame. Simplify MainMenu.cs by removing UIElements/PanelRenderer callback logic and adding StartGame() which loads the "Game" scene via SceneManager.LoadScene. Also add Assets/Scenes/MainMenu.unity to EditorBuildSettings so the scene is included in builds.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,45 +1,12 @@
 using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenu : MonoBehaviour
 {
-    public PanelRenderer panelRenderer;
-
-    int _uiVersion = -1; 
-
-    private void OnEnable()
+    public void StartGame()
     {
-        panelRenderer.RegisterUIReloadCallback();
-    }
-
-    void OnUiReload(PanelRenderer r, VisualElement root, int version)
-    {
-        if (_uiVersion == version)
-        {
-            return;
-        }
-
-        _uiVersion = version;
-
-
-    }
-
-
-    private void OnDisable()
-    {
-        panelRenderer.UnregisterUIReloadCallback();
-    }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Game");
     }
 }
