ed777ac0c109d72a5d5ef22dc56086d87136b311
Rasmus Jensen <github@rasmus.zip>
2026-09-09 11:50:27 +0200

    Update FromLeaderboardToMenu.cs

--- Assets/Scripts/FromLeaderboardToMenu.cs
+++ Assets/Scripts/FromLeaderboardToMenu.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BackToMainMenu : MonoBehaviour
 {
+
+    public Button backButton;
+    private void Start()
+    {
+        backButton.onClick.AddListener(Back);
+    }
     public void Back()
     {
         SceneManager.LoadScene("MainMenu");
